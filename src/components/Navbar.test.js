3a408a39019d3736951108cb2d4154e0ace77276
Liam R <liamroberts77@googlemail.com>
2021-09-08 11:22:04 +0100

    updated folder structure

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -13,9 +13,3 @@ describe('Navbar', () => {
       });
 
 });
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
