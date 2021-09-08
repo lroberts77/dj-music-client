9cbf15d3f9101f108546ffd5989dc88f88f61c4a
Liam R <liamroberts77@googlemail.com>
2021-09-08 19:30:25 +0100

    added images to assets/images

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import "./Navbar.css";
+import vinyl from "../../assets/images/record.png"
+import fractal from "../../assets/images/fractal.png"
 
 const Navbar = () => {
     return (
         <nav className='navbar'>
-            
+            <div className="logo">
+            <img className="img" src={fractal} alt="logo" />
+            <h2 className="owner">FRACTAL</h2>
+            </div>
         </nav>
     )
 }
