bbb0f29ca93542311ca30fb54de0967bcd780524
Liam R <liamroberts77@googlemail.com>
2021-09-12 15:57:15 +0100

    passed logo div rendering test

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -8,8 +8,12 @@ describe('Navbar', () => {
     let wrapper;
     beforeEach(() => wrapper = shallow(<Navbar />));
 
-    it('should render a <div />', () => {
+    it('should render a nav <div />', () => {
         expect(wrapper.find('nav').length).toEqual(1);
       });
+
+    it('should render a <div /> with classname logo', () => {
+      expect(wrapper.find('.logo').length).toEqual(1);
+    });
 
 });
