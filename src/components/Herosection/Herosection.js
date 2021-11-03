63c6f17eab61761b89df554f7bd4dc0d50fa71bb
Liam R <liamroberts77@googlemail.com>
2021-11-03 18:41:22 +0000

    added section2 title

--- src/components/Herosection/Herosection.js
+++ src/components/Herosection/Herosection.js
@@ -37,7 +37,9 @@ export default Herosection => {
             </div>
             <div className="section2">
                 <div className="music-container">
-                    
+                    <div className="music-title">
+                        Listen and enjoy a flake
+                    </div>
                 <ReactPlayer className="music-player" height="200px" width="calc(100vw - 40px)" url='https://soundcloud.com/i_am_fractal/witness-the-fitness-remix?si=673e3b8d8b1c4108a62443decac991a3' />
                 </div>
             </div>
