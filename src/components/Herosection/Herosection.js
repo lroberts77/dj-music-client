f9fe123ef6709f151068c54987a85fafe53b1a54
Liam R <liamroberts77@googlemail.com>
2021-11-03 19:25:14 +0000

    added classnames to music player divs

--- src/components/Herosection/Herosection.js
+++ src/components/Herosection/Herosection.js
@@ -43,7 +43,7 @@ export default Herosection => {
                     <div className="song1">
                         <ReactPlayer className="music-player" height="200px" width="calc(100vw - 40px)" url='https://soundcloud.com/i_am_fractal/witness-the-fitness-remix?si=673e3b8d8b1c4108a62443decac991a3' />
                     </div>
-                    <div>
+                    <div className="song2">
                         <ReactPlayer className="music-player" height="200px" width="calc(100vw - 40px)" url='https://soundcloud.com/i_am_fractal/witness-the-fitness-remix?si=673e3b8d8b1c4108a62443decac991a3' />
                     </div>
                 </div>
