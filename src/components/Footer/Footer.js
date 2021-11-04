bbf64512e195d68dfe5407368cab1600e18a4a79
Liam R <liamroberts77@googlemail.com>
2021-11-04 21:59:33 +0000

    added footer container to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,9 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
+            <div className="footer-container">
+                
+            </div>
             <div>
                 <a className="copyright" href="https://lroberts77.github.io/">&copy; L.Roberts designs 2021</a>
             </div>
