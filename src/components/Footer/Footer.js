3062a8afa9e46d5425f496dd913b53e83ee81412
Liam R <liamroberts77@googlemail.com>
2021-11-05 13:05:19 +0000

    added divs and placeholder for footer list links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,16 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-container">
-                
+                <div className="footer-links">
+                <ul className="footer-ul">
+                    <li>
+                        <a href="">Home</a>
+                        <a href="">Home</a>
+                        <a href="">Home</a>
+                        <a href="">Home</a>
+                    </li>
+                </ul>
+                </div>
             </div>
             <div>
                 <a className="copyright" href="https://lroberts77.github.io/">&copy; L.Roberts designs 2021</a>
