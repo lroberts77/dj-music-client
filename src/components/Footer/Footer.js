8627731d66d04c6199746928f42bb051105e2e4a
Liam R <liamroberts77@googlemail.com>
2021-11-06 18:51:47 +0000

    resized section1 and about-container

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <div>
+            <div className="copyright-container">
                 <a className="copyright" href="https://lroberts77.github.io/">&copy; L.Roberts designs 2021</a>
             </div>
         </div>
